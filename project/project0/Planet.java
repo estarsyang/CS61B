e47cbe42b90e6c5fb9c5e476591cdd75c678c585
estarsyang <455089957@qq.com>
2022-09-25 14:43:54 +0800

    feat: add project 0 skeleton files and demo CS61B code

---
+++ project/project0/Planet.java
@@ -0,0 +1,15 @@
+public class Planet {
+  public double xxPos;
+  public double yyPos;
+  public double xxVel;
+  public double yyVel;
+  public double mass;
+  public String imgFileName;
+
+  public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+
+  }
+  public Planet(Planet P){
+    
+  }
+}
