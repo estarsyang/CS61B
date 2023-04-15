246913ccbec4acb3a94c31093735f4d9054b0dca
estarsyang <455089957@qq.com>
2023-04-15 17:29:50 +0800

    feat: add lecture code 2_1, 2_2

--- lecture code/2.2/SLList_1.java
+++ lecture code/2.2/SLList_1.java
@@ -4,8 +4,10 @@
  * 2. access control
  * 3. nested and static class
  * 4. add more SLList functionality. addLast and size
+ * 5. fast size()
+ * 6. add empty list
  */
-public class SLList {
+public class SLList_1 {
 
     private static class IntNode {
         public int item;
@@ -19,14 +21,22 @@ public class SLList {
 
     /** use private to replace public to prevent outside use first */
     private IntNode first;
+    private int size;
 
-    public SLList(int x){
+    public SLList_1(int x){
         first = new IntNode(x, null);
+        size = 1;
+    }
+
+    public SLList_1(){
+        first = null;
+        size = 0;
     }
 
     /** Add x to the front of the list */
     public void addFirst(int x){
         first = new IntNode(x, first);
+        size += 1;
     }
 
     /** Return the first item in the list */
@@ -36,6 +46,13 @@ public class SLList {
 
     /** add one IntNode to the end of SSList */
     public void addLast(int x){
+        size += 1;
+
+        if(first == null){
+            first = new IntNode(x,null);
+            return;
+        }
+
         IntNode p = first;
         while (p.next != null) {
             p = p.next;
@@ -43,26 +60,24 @@ public class SLList {
         p.next = new IntNode(x, null);
     }
 
-    /** Return the size of the list that start at IntNode p */
-    private int size(IntNode p){
-        if(p.next == null){
-            return 1;
-        }
-        return 1 + size(p.next);
-    }
 
     public int size() {
-        return size(first);
+        return size;
     }
 
     public static void main(String[] args){
         /** Creates a list of one integer, namely 10 */
-        SLList L =  new SLList(10);
-        L.addFirst(15);
-        L.addFirst(5);
-        L.addLast(20);
-
-        System.out.println(L.getFirst());
+//        SLList_1 L =  new SLList_1(10);
+//        L.addFirst(15);
+//        L.addFirst(5);
+//        L.addLast(20);
+//
+//        System.out.println(L.getFirst());
+//        System.out.println(L.size());
+        /** empty list test */
+        SLList_1 L = new SLList_1();
+        L.addLast(10);
+        L.addFirst(20);
         System.out.println(L.size());
     }
 }
--- lecture code/2.2/SLList_2.java
+++ lecture code/2.2/SLList_2.java
@@ -4,8 +4,12 @@
  * 2. access control
  * 3. nested and static class
  * 4. add more SLList functionality. addLast and size
+ * 5. fast size()
+ * 6. add empty list
+ * 7. sentinel Nodes
+ * 8. invariants
  */
-public class SLList {
+public class SLList_2 {
 
     private static class IntNode {
         public int item;
@@ -18,51 +22,60 @@ public class SLList {
     }
 
     /** use private to replace public to prevent outside use first */
-    private IntNode first;
+    /** the first item(if it exists) is at sentinel.next. */
+    private IntNode sentinel;
+    private int size;
 
-    public SLList(int x){
-        first = new IntNode(x, null);
+    public SLList_2(int x){
+        sentinel = new IntNode(63, null);
+        sentinel.next = new IntNode(x, null);
+        size = 1;
+    }
+
+    public SLList_2(){
+        sentinel = new IntNode(63,null);
+        size = 0;
     }
 
     /** Add x to the front of the list */
     public void addFirst(int x){
-        first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next);
+        size += 1;
     }
 
     /** Return the first item in the list */
     public int getFirst(){
-        return first.item;
+        return sentinel.next.item;
     }
 
     /** add one IntNode to the end of SSList */
     public void addLast(int x){
-        IntNode p = first;
+        size += 1;
+
+        IntNode p = sentinel;
         while (p.next != null) {
             p = p.next;
         }
         p.next = new IntNode(x, null);
     }
 
-    /** Return the size of the list that start at IntNode p */
-    private int size(IntNode p){
-        if(p.next == null){
-            return 1;
-        }
-        return 1 + size(p.next);
-    }
 
     public int size() {
-        return size(first);
+        return size;
     }
 
     public static void main(String[] args){
         /** Creates a list of one integer, namely 10 */
-        SLList L =  new SLList(10);
-        L.addFirst(15);
-        L.addFirst(5);
+//        SLList_2 L =  new SLList_2(10);
+//        L.addFirst(15);
+//        L.addFirst(5);
+//        L.addLast(20);
+//
+//        System.out.println(L.getFirst());
+//        System.out.println(L.size());
+        /** empty list test */
+        SLList_2 L = new SLList_2();
         L.addLast(20);
-
-        System.out.println(L.getFirst());
         System.out.println(L.size());
     }
 }
