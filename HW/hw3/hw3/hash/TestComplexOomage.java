700489f73936c66401e500cb74040f41cd47a372
estarsyang <455089957@qq.com>
2023-05-21 22:19:03 +0800

    feat: add hw2 and hw3

---
+++ HW/hw3/hw3/hash/TestComplexOomage.java
@@ -0,0 +1,74 @@
+package hw3.hash;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+
+public class TestComplexOomage {
+
+    @Test
+    public void testHashCodeDeterministic() {
+        ComplexOomage so = ComplexOomage.randomComplexOomage();
+        int hashCode = so.hashCode();
+        for (int i = 0; i < 100; i += 1) {
+            assertEquals(hashCode, so.hashCode());
+        }
+    }
+
+    /* This should pass if your OomageTestUtility.haveNiceHashCodeSpread
+       is correct. This is true even though our given ComplexOomage class
+       has a flawed hashCode. */
+    @Test
+    public void testRandomOomagesHashCodeSpread() {
+        List<Oomage> oomages = new ArrayList<>();
+        int N = 10000;
+
+        for (int i = 0; i < N; i += 1) {
+            oomages.add(ComplexOomage.randomComplexOomage());
+        }
+
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
+    }
+
+    /* TODO: Create a list of Complex Oomages called deadlyList
+     * that shows the flaw in the hashCode function.
+     */
+    // https://blog.csdn.net/qq_45698833/article/details/115769641
+    /**
+     * cuz complexOomage's hashcode is total*256 and plus next number.
+     * since jave int is 32 bit. so largest value is 2^31 -1
+     * as 256 is 2^8. so time it 4 times will exceed max value and dead!
+     */
+
+    @Test
+    public void testWithDeadlyParams() {
+        List<Oomage> deadlyList = new ArrayList<>();
+
+        // Your code here.
+        int deadlyListSize=10000;
+        for (int i=0;i<deadlyListSize;i++){
+            int N = StdRandom.uniform(1, 10);
+            ArrayList<Integer> params = new ArrayList<>(N);
+            for (int j = 0; j < N - 4; j += 1) {
+                params.add(StdRandom.uniform(0, 255));
+            }
+            for (int j = N - 4; j < N; j++) {
+                params.add(1);
+            }
+            deadlyList.add(new ComplexOomage(params));
+        }
+
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
+    }
+
+    /** Calls tests for SimpleOomage. */
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestComplexOomage.class);
+    }
+}
